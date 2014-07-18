4994eb23541f8e2995e21f1a8cc13622a543f6cb
Renato Lorenzi <renato.lorenzi@hotmail.com>
2014-07-18 19:44:07 -0300

    Tests

--- src/test/groovy/rml/ramenos/messager/MessengerTest.groovy
+++ src/test/groovy/rml/ramenos/messager/MessengerTest.groovy
@@ -1,45 +1,41 @@
 package rml.ramenos.messager
-
-import br.com.chat4e.ChatCallback
-import org.junit.Test
-
 /**
  * Created by renatol on 7/11/14.
  */
 class MessengerTest {
 
-
-    @Test
-    public void testMessenger() {
-        def expectedBuddy = new Buddy()
-        def expectedMessage = "xalala"
-        def passed = false
-
-        def messenger = new Messenger(new ChatCallback() {
-
-            @Override
-            void receivedMessage(Buddy buddy, String s) {
-                assert expectedBuddy == buddy
-                assert expectedMessage == s
-                passed = true
-            }
-
-            @Override
-            void sendFailed(String s) {
-
-            }
-        })
-
-        messenger.start()
-
-
-        sleep(2000)
-
-        messenger.sendMessage(expectedBuddy, expectedMessage)
-
-        while (!passed) {
-            sleep(1000)
-        }
-    }
+//    @Ignore
+//    @Test
+//    public void testMessenger() {
+//        def expectedBuddy = new Buddy()
+//        def expectedMessage = "xalala"
+//        def passed = false
+//
+//        def messenger = new Messenger(new ChatCallback() {
+//
+//            @Override
+//            void receivedMessage(Buddy buddy, String s) {
+//                assert expectedBuddy == buddy
+//                assert expectedMessage == s
+//                passed = true
+//            }
+//
+//            @Override
+//            void sendFailed(String s) {
+//
+//            }
+//        })
+//
+//        messenger.start()
+//
+//
+//        sleep(2000)
+//
+//        messenger.sendMessage(expectedBuddy, expectedMessage)
+//
+//        while (!passed) {
+//            sleep(1000)
+//        }
+//    }
 
 }
